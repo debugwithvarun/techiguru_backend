eb2f7b2c9b0d74c85ddc25a4d988fec994b54016
Varunpython028 <varun_mit@techhubtechnology.com>
2026-02-07 22:09:55 +0530

    fix CORS Bug

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    // origin: 'http://localhost:5173', 
+    origin: 'https://techiguru.vercel.app/', 
     credentials: true
 }));
 
